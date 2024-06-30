de6aac39055c6b59d42768acf2185f193ea225f8
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-06-30 11:54:24 +0100

    Update page.jsx

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,9 @@
+import DataList from "@/components/dataList";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
+      <DataList/>
     </main>
   );
 }
