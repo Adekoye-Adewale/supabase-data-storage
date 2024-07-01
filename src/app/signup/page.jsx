e49914238b44466d1d4728040661a9d6a101a247
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-02 00:47:48 +0100

    Create page.jsx

---
+++ src/app/signup/page.jsx
@@ -0,0 +1,10 @@
+import { SignupForm } from '@/components/form'
+import React from 'react'
+
+export default function SignUpPage() {
+    return (
+        <main>
+            <SignupForm/>
+        </main>
+    )
+}
