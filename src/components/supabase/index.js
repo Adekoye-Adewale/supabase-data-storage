7c9b94fb3f4f58f7b1c1a4c1e511d99a9132c75f
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-02 00:48:13 +0100

    Create index.js

---
+++ src/components/supabase/index.js
@@ -0,0 +1,5 @@
+import { FetchAndDisplayData } from "./fetchDataList";
+import { handleSaveData } from "./handleSaveData";
+import { handleConfirmDelete } from "./handleConfirmDelete";
+
+export { FetchAndDisplayData, handleConfirmDelete, handleSaveData };
