8ffc73ff01a88d4afc697e5637d2a45449477bc7
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-03 15:35:11 +0100

    Create fetchUser.js

---
+++ src/components/supabase/fetchUser.js
@@ -0,0 +1,39 @@
+import { supabase } from '../supabase/supabaseClient';
+
+export const FetchUserProfile = async () => {
+    const { data: profile, error } = await supabase
+        .from('profiles')
+        .select('user_name, full_name, email, user_avatar, cover_img')
+        .single();
+
+    if (error) {
+        console.error('Error fetching user profile:', error);
+        return null;
+    }
+
+    return profile;
+};
+
+export async function UserAvatar() {
+    let userProfile = null;
+    try {
+        const { data: { session } } = await supabase.auth.getSession();
+        if (session) {
+            const { data, error } = await supabase
+                .from('user_profiles')
+                .select('user_name, full_name, email, user_avatar, cover_img')
+                .eq('id', session.user.id)
+                .single();
+
+            if (error) {
+                throw error;
+            }
+
+            userProfile = data;
+        }
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+    }
+    
+    return userProfile;
+}
