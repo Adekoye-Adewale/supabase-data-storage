4817ea51c65731bbc1b27ada229741bdec9a438e
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-04 06:21:11 +0100

    Update index.jsx

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,39 +1,9 @@
-'use client'
-import React, { useEffect, useState } from 'react'
-import { supabase } from '@/app/utils/supabase';
-import SiteLogo from './logo'
-import Link from 'next/link'
-import LogoutButton from './logOutBtn';
-import { UserAvatar } from '../dashboard';
+import React from 'react';
+import SiteLogo from './logo';
+import NavMessage from './navMessage';
+import NavMenu from './navMenu';
 
 export default function SiteHeader() {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const fetchUser = async () => {
-            try {
-                const { data: { session } } = await supabase.auth.getSession();
-                if (session) {
-                    const { data: userProfile } = await supabase
-                        .from('user_profiles')
-                        .select('user_name')
-                        .eq('id', session.user.id)
-                        .single();
-
-                    if (userProfile) {
-                        setUser({ ...session.user, user_name: userProfile.user_name });
-                    }
-                }
-            } catch (error) {
-                console.error('Error fetching user data:', error);
-            } finally {
-                setLoading(false);
-            }
-        };
-
-        fetchUser();
-    }, []);
 
     return (
         <header>
@@ -41,21 +11,8 @@ export default function SiteHeader() {
                 className="flex justify-between items-center p-4 bg-gray-800 text-white"
             >
                 <SiteLogo />
-                <nav>
-                    {loading ? (
-                        <span>Loading...</span>
-                    ) : user ? (
-                        <div className="flex items-center space-x-4">
-                            <span>Welcome, {user.user_name}</span>
-                            <UserAvatar/>
-                            <LogoutButton />
-                        </div>
-                    ) : (
-                        <Link href={'/login'} className="text-white">
-                            Login
-                        </Link>
-                    )}
-                </nav>
+                <NavMenu/>
+                <NavMessage/>
             </div>
         </header>
     );
