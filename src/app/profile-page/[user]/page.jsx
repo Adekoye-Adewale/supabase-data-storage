fdb5c33a00b23a976f711443aec13542aea12c4b
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-04 06:20:09 +0100

    Update page.jsx

--- src/app/profile-page/[user]/page.jsx
+++ src/app/profile-page/[user]/page.jsx
@@ -1,48 +1,43 @@
 import React from 'react';
-import { createClient as supabase } from '@/app/utils/supabase/client';
+import { supabase } from '@/app/utils/supabase';
+import ProfilePageHeader from '@/components/dashboard/header';
+import { UserProfile } from '@/components/dashboard';
+
 
 export default async function UserPage({ params }) {
-  const { user } = params; // Assuming `user` is the identifier
-  let userProfile = null;
-  let error = null;
+    const { user } = params; 
+    let userProfile = null;
+    let error = null;
 
-  try {
-    // Directly query the user profile based on the provided identifier
-    const { data: userProfileData, error: profileError } = await supabase
-      .from('user_profiles')
-      .select('cover_img, user_avatar, email, full_name, phone_number, user_name')
-      .eq('user_name', user) // Assuming `user` is the username
-      .single();
+    try {
+        const { data: userProfileData, error: profileError } = await supabase
+        .from('user_profiles')
+        .select('cover_img, user_avatar, email, full_name, phone_number, user_name')
+        .eq('user_name', user)
+        .single();
 
-    if (profileError) {
-      throw new Error(`Profile fetch error: ${profileError.message}`);
-    } else {
-      userProfile = userProfileData;
+        if (profileError) {
+        throw new Error(`Profile fetch error: ${profileError.message}`);
+        } else {
+        userProfile = userProfileData;
+        }
+    } catch (err) {
+        error = err;
     }
-  } catch (err) {
-    error = err;
-  }
 
-  console.log('=======>',userProfile);
+    if (error) {
+        return <div>Error fetching user data: {error.message}</div>;
+    }
 
-  if (error) {
-    return <div>Error fetching user data: {error.message}</div>;
-  }
+    if (!userProfile) {
+        return <div>No user profile found.</div>;
+    }
 
-  if (!userProfile) {
-    return <div>No user profile found.</div>;
-  }
-
-  return (
-    <div>
-      <h1>{params.user}</h1>
-      <p>
-        <span>Username:</span> {userProfile.user_name}
-      </p>
-      <p>
-        <span>Phone Number:</span> {userProfile.phone_number} (optional: handle privacy)
-      </p>
-      {/* Add other profile details here */}
-    </div>
-  );
+    return (
+        <>
+            <UserProfile       
+                profile={userProfile}
+            />
+        </>
+    );
 }
