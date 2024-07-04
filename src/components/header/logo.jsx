cea66f311e51f3b361c8437a2ec8f00e8f3fb71f
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-04 06:21:08 +0100

    Update logo.jsx

--- src/components/header/logo.jsx
+++ src/components/header/logo.jsx
@@ -1,16 +1,17 @@
-import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function SiteLogo() {
     return (
-        <div>
+        <Link href={'/'}>
             <Image 
-                src={'/images/akanda_dev.png'} 
+                src={'/images/logo.png'} 
                 alt={'logo'} 
                 width={300} 
                 height={300}
-                className='w-24 h-24'
+                className='w-20 h-20'
             />
-        </div>
+        </Link>
     )
 }
