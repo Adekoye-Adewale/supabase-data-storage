c126e7b0510a4e9ffa45a0c20a376d0b9fc30e33
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-06 15:05:27 +0100

    Update page.jsx

--- src/app/users/page.jsx
+++ src/app/users/page.jsx
@@ -16,18 +16,20 @@ export default async function UsersPage() {
     }
 
     return (
-        <main>
-            {data.map((user) => (
-                <div key={user._id} className='divide'>
-                    <UserCard 
-                        img={user.user_avatar} 
-                        userName={user.user_name} 
-                        fullName={user.full_name} 
-                        email={user.email} 
-                        phoneNumber={user.phone_number}
-                    />
-                </div>
-            ))}
+        <main className='p-4'>
+            <div className=''>
+                {data.map((user) => (
+                    <div key={user.id} className='my-4'>
+                        <UserCard 
+                            img={user.user_avatar} 
+                            userName={user.user_name} 
+                            fullName={user.full_name} 
+                            email={user.email} 
+                            phoneNumber={user.phone_number}
+                        />
+                    </div>
+                ))}
+            </div>
         </main>
     )
 }
@@ -39,23 +41,23 @@ const UserCard = ({ img, userName, fullName, email, phoneNumber}) => {
             <Link 
                 href={`/${userName}`} 
                 title={userName}
-                className='flex'
+                className='flex gap-2 rounded-md shadow-md w-full max-w-[400px] p-5 bg-gray-800 hover:bg-sky-700 transition-all duration-300'
             >
-                <Image {...img} className='w-4 h-4 rounded-full' />
+                <Image {...img} className='size-12 border rounded-full' />
                 <div>
-                    <div>
-                        <h3>
+                    <div className='flex gap-2 items-baseline'>
+                        <h3 className='text-xl'>
                             {fullName}
                         </h3>
-                        <span>
-                            {userName}
+                        <span className='text-[8px] italic'>
+                            @{userName}
                         </span>
                     </div>
-                    <div>
-                        <sapn>
+                    <div className='flex gap-2'>
+                        <sapn className='text-xs'>
                             {email}
                         </sapn>
-                        <span>
+                        <span className='text-xs'>
                             {phoneNumber}
                         </span>
                     </div>
