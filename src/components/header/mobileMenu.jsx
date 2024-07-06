0e3b45863e86f32bb691f577008b69ffc3a1073a
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-06 15:45:59 +0100

    Create mobileMenu.jsx

---
+++ src/components/header/mobileMenu.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MenuIcons from './menuIcons'
+
+export default function MobileMenu() {
+    return (
+        <div>
+            <MenuIcons/>
+        </div>
+    )
+    }
