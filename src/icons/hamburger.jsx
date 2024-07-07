22ea7e967b6ac8fc1530162f07b02d711a60e2f1
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-07 18:39:21 +0100

    Update hamburger.jsx

--- src/icons/hamburger.jsx
+++ src/icons/hamburger.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Hamburger() {
     return (
-        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
+        <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" className="h-6 w-6 cursor-pointer text-current">
             <path fill="currentColor" fillRule="evenodd" d="M20.75 7a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75m0 5a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75m0 5a.75.75 0 0 1-.75.75H4a.75.75 0 0 1 0-1.5h16a.75.75 0 0 1 .75.75" clipRule="evenodd"></path>
         </svg>
     )
