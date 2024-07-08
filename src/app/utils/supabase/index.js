480605cc4a8024a3135591f3a32c1c2fdd5ec5cd
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-08 21:58:30 +0100

    Update index.js

--- src/app/utils/supabase/index.js
+++ src/app/utils/supabase/index.js
@@ -2,5 +2,6 @@ import supabase from '@/app/utils/supabase/client';
 import { FetchAndDisplayData } from "./fetchDataList";
 import { handleSaveData } from "./handleSaveData";
 import { handleConfirmDelete } from "./handleConfirmDelete";
+import { UserData } from './fetchUser';
 
-export { supabase, FetchAndDisplayData, handleConfirmDelete, handleSaveData };
+export { supabase, FetchAndDisplayData, handleConfirmDelete, handleSaveData, UserData };
