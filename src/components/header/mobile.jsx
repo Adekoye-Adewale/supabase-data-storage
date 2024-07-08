38e9ea93c4d0add4bed028db193167d7b2a916f1
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-08 22:01:04 +0100

    Create mobile.jsx

---
+++ src/components/header/mobile.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import MobileMenu from './mobileMenu';
+import { useFetchUserData } from '@/app/utils/supabase/useFetchUserData';
+
+export default function Mobile() {
+    
+    const { data, loading } = useFetchUserData();
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return (
+        <div>
+            <MobileMenu {...data} />
+        </div>
+    );
+}
