54f2f6d3406ee6ab07a4a25b8d7d43fa590954d9
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-08 21:57:40 +0100

    Update fetchUser.js

--- src/app/utils/supabase/fetchUser.js
+++ src/app/utils/supabase/fetchUser.js
@@ -37,3 +37,32 @@ export async function UserAvatar() {
     
     return userProfile;
 }
+
+export async function UserData() {
+    try {
+        const { data: { session }, error: sessionError } = await supabase.auth.getSession();
+        
+        if (sessionError) {
+            throw new Error('Session retrieval failed');
+        }
+
+        if (!session) {
+            return null;
+        }
+
+        const { data, error } = await supabase
+            .from('user_profiles')
+            .select('user_name, full_name, email, phone_number, user_avatar, cover_img')
+            .eq('id', session.user.id)
+            .single();
+
+        if (error) {
+            throw error;
+        }
+
+        return data;
+    } catch (error) {
+        console.error('Error fetching user data:', error);
+        return null;
+    }
+}
