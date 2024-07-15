d3a384e6efa9f9389ee8760c0cb2065e4cb789e2
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-07-15 20:46:37 +0100

    Update page.jsx

--- src/app/my-account/page.jsx
+++ src/app/my-account/page.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
 import { createClientSession } from '../utils/supabase/server'
 import ProfilePageHeader from '@/components/dashboard/header'
+import Link from 'next/link'
 
 export default async function myAccountPage() {
 
     const { data, error } = await createClientSession()
 
     if (error) {
-        return <div>Something went wrong while fetching user data: {error.message}</div>;
+        return (
+            <div>
+                <span className='m-auto text-3xl text-center font-bold'>
+                    Something went wrong while fetching user data: {error.message}
+                </span>
+                <div>
+                    <Link href={'/login'} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-5 rounded-sm'>
+                        Login
+                    </Link>
+                </div>
+            </div>
+        )
     }
 
     return (
