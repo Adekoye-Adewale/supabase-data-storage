e02801bed1e7735ef002bcc92776106c60a0429f
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-16 18:59:02 +0100

    Create photoIcon.jsx

---
+++ src/icons/photoIcon.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function PhotoIcon() {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24">
+      <g fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={1}>
+        <path d="M15 8h.01M3 6a3 3 0 0 1 3-3h12a3 3 0 0 1 3 3v12a3 3 0 0 1-3 3H6a3 3 0 0 1-3-3z"></path>
+        <path d="m3 16l5-5c.928-.893 2.072-.893 3 0l5 5"></path>
+        <path d="m14 14l1-1c.928-.893 2.072-.893 3 0l3 3"></path>
+      </g>
+    </svg>
+  )
+}
