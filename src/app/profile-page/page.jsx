2790b13bacad43d92b7e2d87ee05aa138a5c7d39
Adekoye Adewale <adekoyeadewale@gmail.com>
2024-08-21 00:26:53 +0100

    Update page.jsx

--- src/app/profile-page/page.jsx
+++ src/app/profile-page/page.jsx
@@ -22,34 +22,56 @@ export default function ProfilePage() {
     return (
         <main>
             {isLoggedIn ? (
-                <UserProfile profile={user} />
+                <>
+                    <UserProfile profile={user} />
+                    <BioData userBio={user}/>
+                </>
             ) : (
-                <div className='p-2 w-full min-h-[500px] grid place-content-center gap-2 text-center'>
-                    <span className='m-auto text-3xl text-center font-bold'>
-                        User needs to sign in
-                    </span>
-                    <div className='my-8'>
-                        <Link 
-                            href={'/login'} 
-                            className='text-base font-semibold text-sky-950 bg-slate-500 py-3 px-7 rounded-md hover:bg-slate-300 transition-all duration-300'
-                        >
-                            Login now
-                        </Link>
-                    </div>
-                </div>
-                
+                <SignInNotice/>
             )}
-            <div className="rounded-lg border border-gray-900/10 pb-12 space-y-12 bg-slate-800 max-w-4xl my-5 mx-auto px-5 py-10">
-                <div>
-                    <h2 className="text-3xl font-semibold leading-7 text-white">
-                        Profile
-                    </h2>
-                    <p className="mt-1 text-sm leading-6 text-gray-600">
-                        This information will be displayed publicly so be careful what you share.
-                    </p>
-                </div>
-                <UserProfileForm/>
-            </div>
         </main>
     );
+}
+
+
+const BioData = ({ userBio }) => {
+    return (
+        <div className="rounded-lg border border-gray-900/10 pb-12 space-y-12 bg-slate-800 max-w-4xl my-5 mx-auto px-5 py-10">
+            <div>
+                <h2 className="text-3xl font-semibold leading-7 text-white">
+                    Profile
+                </h2>
+                <p className="mt-1 text-sm leading-6 text-gray-600">
+                    This information will be displayed publicly so be careful what you share.
+                </p>
+            </div>
+            <UserProfileForm
+                fullName={userBio.full_name}
+                userName={userBio.user_name}
+                email={userBio.email}
+                userAvatar={userBio.user_avatar.src}
+                coverImg={userBio.cover_img.src}
+                userRole={userBio.role_id}
+                telNumber={userBio.phone_number}
+            />
+        </div>
+    )
+}
+
+const SignInNotice = () => {
+    return (
+        <div className='p-2 w-full min-h-[500px] grid place-content-center gap-2 text-center'>
+            <span className='m-auto text-3xl text-center font-bold'>
+                User needs to sign in
+            </span>
+            <div className='my-8'>
+                <Link
+                    href={'/login'}
+                    className='text-base font-semibold text-sky-950 bg-slate-500 py-3 px-7 rounded-md hover:bg-slate-300 transition-all duration-300'
+                >
+                    Login now
+                </Link>
+            </div>
+        </div>
+    )
 }
